af563c98385fd16657194d654b00f88f6983eb1b
Pedro Bruno <pbruno1283@gmail.com>
2026-02-25 14:10:41 -0300

    Criado fluxo inicial para ler uma Tag NFC

--- feature-nfc/src/main/java/com/tapflowfeature_nfc/NfcDispatchActivity.kt
+++ feature-nfc/src/main/java/com/tapflowfeature_nfc/NfcDispatchActivity.kt
@@ -1,2 +1,35 @@
 package com.tapflowfeature_nfc
 
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+const val TAG = "TapFlow-NFC"
+
+class NfcDispatchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        handleIntent(intent = intent)
+        finish()
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+        finish()
+    }
+
+    private fun handleIntent(intent: Intent?) {
+        if (intent?.action != NfcAdapter.ACTION_TAG_DISCOVERED) return
+
+        val tagId = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID) ?: return
+
+        val uid = tagId.joinToString(":") { byte ->
+            "%02X".format(byte)
+        }
+
+        Log.d(TAG, "NFC TAG DETECTED: $uid")
+    }
+}
